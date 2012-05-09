e9d4e5e91353e6219e82446c4c85d285211392f4
Dmitry Mozzherin <dmozzherin@gmail.com>
2012-05-09 17:09:14 -0400

    home page is sort of working

--- application.rb
+++ application.rb
@@ -1,8 +1,14 @@
 #!/usr/bin/env ruby
-
 require 'sinatra'
 require File.join(File.dirname(__FILE__), 'environment')
 
+set :haml, :format => :html5
+
+get '/main.css' do
+  content_type 'text/css', :charset => 'utf-8'
+  scss :main
+end
+
 get "/" do
- 'hi'
+  haml :home
 end
