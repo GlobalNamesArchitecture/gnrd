1b7c61fd0f940c6bd7423e61477d6f070838f7be
Dmitry Mozzherin <dmozzherin@gmail.com>
2016-03-28 04:24:44 -0500

    specs for all api parameters

--- spec/features/api_spec.rb
+++ spec/features/api_spec.rb
@@ -37,6 +37,25 @@ describe "api" do
                            detect_language: false,
                            engine: 1)
     end
+
+    it "sets parmeters with 0 and 1 too" do
+      params = [%w(unique 1), %w(return_content 1),
+                %w(all_data_sources 1), %w(best_match_only 1),
+                %w(data_source_ids 1|2|3), %w(preferred_data_sources 1|2),
+                %w(detect_language 0), %w(engine 1)
+      ].each_with_object([]) { |(k, v), obj| obj << "#{k}=#{v}" }.join("&")
+      get "/name_finder.json?#{URI.escape(params)}"
+      params = JSON.parse(last_response.body,
+                          symbolize_names: true)[:parameters]
+      expect(params).to eq(unique: true,
+                           return_content: true,
+                           all_data_sources: true,
+                           best_match_only: true,
+                           data_source_ids: [1, 2, 3],
+                           preferred_data_sources: [1, 2],
+                           detect_language: false,
+                           engine: 1)
+    end
   end
 
   context "engines" do
@@ -140,6 +159,66 @@ describe "api" do
             { id: 4, title: "NCBI" }
           ])
         expect(res[:resolved_names].size).to eq 2
+      end
+
+      it "tramps app_data_sources setting" do
+        params = "text=#{text}&data_source_ids=#{URI.escape('1|4')}"
+        params << "&all_data_sources=true"
+        get("/name_finder.json?#{params}")
+        follow_redirect!
+        res = JSON.parse(last_response.body, symbolize_names: true)
+        expect(res[:data_sources]).to eq(
+          [
+            { id: 1, title: "Catalogue of Life" },
+            { id: 4, title: "NCBI" }
+          ])
+        expect(res[:resolved_names].size).to eq 2
+      end
+    end
+
+    context "best_match_only" do
+      it "returns many results when false" do
+        params = "text=#{text}&all_data_sources=true"
+        get("/name_finder.json?#{params}")
+        follow_redirect!
+        res = JSON.parse(last_response.body, symbolize_names: true)
+        expect(res[:resolved_names][0][:results].size).to be > 1
+      end
+
+      it "returns only best-scored match when true" do
+        params = "text=#{text}&best_match_only=true&all_data_sources=true"
+        get("/name_finder.json?#{params}")
+        follow_redirect!
+        res = JSON.parse(last_response.body, symbolize_names: true)
+        expect(res[:resolved_names][0][:results].size).to eq 1
+      end
+    end
+
+    context "preferred_data_sources" do
+      it "returns data from preferred data sources if exist" do
+        params = "text=#{text}&best_match_only=true"
+        params << "&all_data_sources=true&preferred_data_sources=11"
+        get("/name_finder.json?#{params}")
+        follow_redirect!
+        res = JSON.parse(last_response.body, symbolize_names: true)
+        expect(res[:resolved_names][0][:preferred_results].size).to be > 0
+      end
+
+      it "doesn't work with data_source_ids if preferred not included there" do
+        params = "text=#{text}&best_match_only=true"
+        params << "&data_source_ids=1&preferred_data_sources=11"
+        get("/name_finder.json?#{params}")
+        follow_redirect!
+        res = JSON.parse(last_response.body, symbolize_names: true)
+        expect(res[:resolved_names][0][:preferred_results].size).to be 0
+      end
+
+      it "doesn't exist when not set" do
+        params = "text=#{text}&all_data_sources=true"
+        get("/name_finder.json?#{params}")
+        follow_redirect!
+        res = JSON.parse(last_response.body, symbolize_names: true)
+        expect(res[:resolved_names][0][:preferred_results]).to be nil
       end
     end
   end
